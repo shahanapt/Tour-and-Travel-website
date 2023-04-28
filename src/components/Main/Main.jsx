6b82f5fdb54fa403414f2789162aeb85edbd0884
shahana pt <fshahanapt@gmail.com>
2023-04-28 14:47:47 +0530

    next commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./main.css";
 import img from "../../Assets/img.jpeg";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
 
 const Data = [
   {
@@ -109,7 +111,36 @@ const Main = () => {
             return (
               <div key={id} className="singleDestination">
                 <div className="imageDiv">
-                  <img src={img} alt="" />
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+
+                    <div className="price">
+                      <span>{fees}</span>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
                 </div>
               </div>
             );
