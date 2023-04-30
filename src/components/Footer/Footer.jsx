e04269993872928f52d90a5dda7ae1acf8642440
shahana pt <fshahanapt@gmail.com>
2023-04-30 22:21:18 +0530

    next commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,94 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.scss";
+import video2 from "../../Assets/ocean-65560.mp4";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { FiChevronRight } from "react-icons/fi";
+import {
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
 
-export default Footer
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter Email Address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="/page" className="logo flex">
+                <MdOutlineTravelExplore className="icon" />
+                Travel.
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequa
+            </div>
+
+            <div className="footerSocials">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
